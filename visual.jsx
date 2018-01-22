ce07acea9f4ff07437cda2824984c1cc6d0dab08
Anna <annaoh1994@gmail.com>
2018-01-22 13:03:45 -0800

    made legends via vanilla JS dom manipulation

--- visual.jsx
+++ visual.jsx
@@ -5,7 +5,8 @@ import * as scaleChromatic from "d3-scale-chromatic";
 
 
 const width = 960;
-const height = 600;
+const height = 700;
+
 
 var svg = d3.select("svg")
             .attr("width", width)
@@ -14,6 +15,11 @@ var svg = d3.select("svg")
 var path = d3.geoPath();
 
 var x = d3.scaleLinear().domain([40, 60]).rangeRound([600, 860]);
+
+
+const legendValues = [];
+
+
 
 
 // going through data to manipulate it
@@ -62,6 +68,7 @@ function ready(error, us, percentOfPop) {
 // making the state map
   svg.append("g")
       .attr("class", "us")
+      .attr("x", "100")
       .selectAll("path")
       .data(topojson.feature(us, us.objects.states).features)
       .enter().append("path")
@@ -70,7 +77,7 @@ function ready(error, us, percentOfPop) {
       })
       .attr("d", path)
       .attr("stroke", "white")
-      .attr("stroke-width", "1");
+      .attr("stroke-width", "2");
 
 
   console.log(data);
@@ -92,39 +99,49 @@ function ready(error, us, percentOfPop) {
 
 
 // color legend
-  const legendWidth = 300;
-  const legendHeight = 8;
-  const legendY = 0;
-  const legendX = 100;
+  const legendWidth = 20;
+  const legendHeight = 20;
+  const legendY = 680;
+  const legendX = 50;
+  const legendXSpace = 80;
   const legendTickY = 13;
+
 
   let colorIdx = 0;
   while (colorIdx < numColors) {
     svg.append("rect")
         .attr("class", "legendRect")
-        .attr("x", legendX + (colorIdx * legendWidth/numColors) )
+        .attr("x", legendX + (colorIdx * (legendWidth + legendXSpace)))
         .attr("y", legendY)
         .attr("height", legendHeight)
-        .attr("width", legendWidth/numColors)
-        .attr("fill", colorScheme[colorIdx] );
-    svg.append("g")
-        .attr("class", "tick")
-        .attr("x", legendX + (colorIdx * legendWidth/numColors))
-        .append("hr")
-        // .attr("x", legendX + (colorIdx * legendWidth/numColors))
-        .attr("stroke", "#000")
-        .attr("width", 1)
-        .attr("height", legendTickY)
+        .attr("width", legendWidth)
+        .attr("fill", colorScheme[colorIdx] )
+        .attr("border", "1px solid black");
 
-        .append("text")
-        .text("testing")
-        .attr("fill", "#000")
-        .attr("font-size", "0.71em");
-
-    console.log(colorIdx);
     colorIdx += 1;
-
   }
 
+  // added Legend Title
+  var graph = document.getElementsByClassName("graph");
+  graph = graph[0];
+  let legendTitle = document.createElement("div");
+  legendTitle.innerHTML = "Percent of Population";
+  legendTitle.className = "legendTitle";
+  graph.appendChild(legendTitle);
 
+  let legendTextHolder = document.createElement("div");
+  legendTextHolder.className = "legendTextHolder";
+  legendTitle.appendChild(legendTextHolder);
+
+  // added Legend Text
+  colorIdx = 0;
+  let legendText = document.createElement("div");
+  while (colorIdx < numColors) {
+    let cln = legendText.cloneNode(true);
+    cln.innerHTML = `${(Math.round((domainMin + (increment * colorIdx))*10) /10).toFixed(1) } to ${(Math.round((domainMin + (increment * (colorIdx+1)))*10)/10).toFixed(1) }`;
+    cln.className = `legendText item${colorIdx}`;
+    legendTextHolder.appendChild(cln);
+
+    colorIdx += 1;
+  }
 }
